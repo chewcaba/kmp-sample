679def1189d12f816e768908497d0bdee5a07e4c
chewcaba <chewcabba@gmail.com>
2020-08-27 09:53:41 -0500

    add android, ios, and shared projects

---
+++ ios/kmpsampleTests/kmpsampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  kmpsampleTests.swift
+//  kmpsampleTests
+//
+//  Created by A.J. Cabanatuan on 8/27/20.
+//  Copyright © 2020 A.J. Cabanatuan. All rights reserved.
+//
+
+import XCTest
+@testable import kmpsample
+
+class kmpsampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
