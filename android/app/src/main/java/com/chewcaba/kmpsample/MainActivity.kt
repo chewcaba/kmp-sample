dc0e820e3edfc2e1b4c4e779e210770476c6b89d
chewcaba <chewcabba@gmail.com>
2020-09-01 10:02:56 -0500

    able to display text retrieved from kotlin lib

--- android/app/src/main/java/com/chewcaba/kmpsample/MainActivity.kt
+++ android/app/src/main/java/com/chewcaba/kmpsample/MainActivity.kt
@@ -2,10 +2,13 @@ package com.chewcaba.kmpsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.chewcaba.lib.createApplicationScreenMessage
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView.text = createApplicationScreenMessage()
     }
 }
