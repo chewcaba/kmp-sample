fb79b17d2bcc312d75b1dc09d7b24206881e835a
chewcaba <chewcabba@gmail.com>
2020-09-04 10:19:14 -0500

    test ui for ios

--- ios/kmpsample/ContentView.swift
+++ ios/kmpsample/ContentView.swift
@@ -12,9 +12,39 @@ import iosFramework
 struct ContentView: View {
     
     var greeting = Greeting()
+    @State var username = ""
+    @State var password = ""
     
     var body: some View {
-        Text("Hello, World! \(greeting.message())")
+        VStack() {
+            Spacer()
+            Text("Welcome, Please Login")
+                .foregroundColor(Color.white)
+            TextField("Username", text: self.$username)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal, 20)
+                .padding(.vertical, 5)
+            TextField("Password", text: self.$password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal, 20)
+                .padding(.vertical, 5)
+            Button(action: {
+                    
+            }, label: {
+                Text("Login")
+            }).frame(minWidth: 0, maxWidth: .infinity)
+                .padding(10)
+                .background(Color.green)
+                .foregroundColor(Color.white)
+                .cornerRadius(30)
+                .padding(.horizontal, 20)
+            Spacer()
+        }.frame(minWidth: 0,
+                maxWidth: .infinity,
+                minHeight: 0,
+                maxHeight: .infinity,
+                alignment: .topLeading
+        ).background(Color.purple)
     }
 }
 
