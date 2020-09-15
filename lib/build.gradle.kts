4d17c308b85861b1a395a8cb0d621d540f603c0f
chewcaba <chewcabba@gmail.com>
2020-09-15 15:00:32 -0500

    able to import android project again

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,7 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.*
-
-val iosFrameworkName = "SharedFramework"
-
 plugins {
     kotlin("multiplatform") version "1.4.0"
     id("com.android.library")
@@ -10,7 +6,13 @@ plugins {
     kotlin("native.cocoapods") version "1.4.0"
 }
 group = "com.chewcaba"
-version = "1.0"
+version = "1.0.1"
+
+publishing {
+    repositories {
+        mavenLocal()
+    }
+}
 
 repositories {
     gradlePluginPortal()
@@ -19,37 +21,11 @@ repositories {
     mavenCentral()
 }
 
-android {
-    compileSdkVersion(29)
-    defaultConfig {
-        minSdkVersion(21)
-        targetSdkVersion(29)
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
-        getByName("debug") {
-            isMinifyEnabled = false
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-}
-
 kotlin {
-    android() {
+    android {
         publishAllLibraryVariants()
     }
-
-    val iosX64 = iosX64("ios")
-    val iosArm64 = iosArm64("iosArm64")
+    ios()
 
     sourceSets {
         val commonMain by getting
@@ -59,21 +35,13 @@ kotlin {
                 implementation(kotlin("test-annotations-common"))
             }
         }
-
         val androidMain by getting {
             dependencies {
                 implementation("androidx.core:core-ktx:1.2.0")
             }
         }
-
         val androidTest by getting
-
-        val iosMain by getting {
-            dependsOn(commonMain)
-        }
-        val iosArm64Main by getting {
-            dependsOn(iosMain)
-        }
+        val iosMain by getting
         val iosTest by getting
     }
 
@@ -84,17 +52,22 @@ kotlin {
         ios.deploymentTarget = "9.0"
         // You can change the name of the produced framework.
         // By default, it is the name of the Gradle project.
-        frameworkName = iosFrameworkName
+        frameworkName = "SharedFramework"
         podfile = project.file("../ios/Podfile")
     }
 }
 
-publishing {
-    repositories {
-        mavenLocal()
-        maven {
-            name = "KmpSample"
-            url = uri("file://${buildDir}/repo")
+android {
+    compileSdkVersion(29)
+    defaultConfig {
+        minSdkVersion(21)
+        targetSdkVersion(29)
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
         }
     }
 }
