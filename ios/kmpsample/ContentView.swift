44a60bedff8765a735f455d15d77a11543b284ec
chewcaba <chewcabba@gmail.com>
2020-09-16 11:41:05 -0500

    able to use LoginService in ios project

--- ios/kmpsample/ContentView.swift
+++ ios/kmpsample/ContentView.swift
@@ -11,8 +11,10 @@ import SharedFramework
 
 struct ContentView: View {
     
-    @State var username = ""
-    @State var password = ""
+    @State private var username = ""
+    @State private var password = ""
+    @State private var showingAlert = false
+    @State private var isLoginSuccessfulText = "false" // can't use bool with string interpolation?
     
     var loginService = LoginServiceFactory().makeInstance()
     
@@ -30,16 +32,34 @@ struct ContentView: View {
                 .padding(.horizontal, 20)
                 .padding(.vertical, 5)
             Button(action: {
-                    
+                var isLoginSuccessful = false
+                do {
+                    let login = try self.loginService.login(username: self.username, password: self.password)
+                    if let result = login as? ResultSuccess {
+                        isLoginSuccessful = result.data as! Bool
+                    } else {
+                        isLoginSuccessful = false
+                    }
+                } catch {
+                    isLoginSuccessful = false
+                }
+                self.isLoginSuccessfulText = isLoginSuccessful
+                    ? "true"
+                    : "false"
+                self.showingAlert = true
             }, label: {
                 Text("Login")
-            }).frame(minWidth: 0, maxWidth: .infinity)
+            })
+            .frame(minWidth: 0, maxWidth: .infinity)
                 .padding(10)
                 .background(Color.green)
                 .foregroundColor(Color.white)
                 .cornerRadius(30)
                 .padding(.horizontal, 20)
             Spacer()
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text("Alert"), message: Text("Was Login Successful? \(isLoginSuccessfulText)"), dismissButton: .default(Text("Got it!")))
+            }
         }.frame(minWidth: 0,
                 maxWidth: .infinity,
                 minHeight: 0,
